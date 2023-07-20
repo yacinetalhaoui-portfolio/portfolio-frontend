7392945f65b846e2323b2823fe2da8b277f1369a
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-20 21:09:16 +0200

    :sparkles: Ajout du projet LGM dans le squelette

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -9,27 +9,51 @@ const Project = () => {
     const project = location.state;
 
     return (
-        <div className="grid max-h-screen grid-cols-[1fr_3fr]">
-            <Sidebar/>
-            <div className="max-h-screen overflow-y-scroll">
+        <div className="grid grid-cols-[1fr_3fr]">
+            <Sidebar />
+            <div className="h-screen overflow-y-scroll">
                 <Navbar/>
                 <div className="my-10 px-10">
-                    <h1 className="mb-10 text-white text-3xl text-left">{project.name}
+                    <a href={project.github_link} className="text-white text-3xl text-left">
+                        <div className="flex gap-2 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30">
+                                <g fill="#36c">
+                                    <path d="M20.437 2.69c-3.37 0-5.778 3.05-8.186 5.297.322 0 .804-.16 1.285-.16.803 0 1.605.16 2.408.48 1.284-1.283 2.568-2.727 4.494-2.727.963 0 2.087.48 2.89 1.123 1.605 1.605 1.605 4.174 0 5.78l-4.174 4.172c-.642.642-1.926 1.124-2.89 1.124-2.246 0-3.37-1.446-4.172-3.212l-2.086 2.087c1.284 2.408 3.21 4.173 6.1 4.173 1.926 0 3.69-.802 4.815-2.086l4.172-4.174c1.445-1.444 2.408-3.21 2.408-5.297-.32-3.53-3.53-6.58-7.063-6.58z"/>
+                                    <path d="M13.535 22.113l-1.444 1.444c-.64.642-1.925 1.124-2.89 1.124-.962 0-2.085-.48-2.888-1.123-1.605-1.605-1.605-4.334 0-5.778l4.174-4.175c.642-.642 1.926-1.123 2.89-1.123 2.246 0 3.37 1.605 4.172 3.21l2.087-2.087c-1.284-2.407-3.21-4.173-6.1-4.173-1.926 0-3.692.803-4.815 2.087L4.547 15.69c-2.73 2.73-2.73 7.063 0 9.63 2.568 2.57 7.062 2.73 9.47 0l3.05-3.05c-.482.162-.963.162-1.445.162-.803 0-1.445 0-2.087-.32z"/>
+                                </g>
+                            </svg>
+                            <p>{project.name}</p>
+                        </div>
                         <hr className="my-4 border-t-2 border-gray-300 w-[60%]"/>
-                    </h1>
+                    </a>
                     <div className="flex flex-col gap-5">
                         <div className="flex flex-col gap-5">
                             <span className="text-secondary text-2xl">Contexte : {project.context}</span>
                             <span className="text-secondary text-xl">Taille de l'équipe : {project.team_size}</span>
                             <span className="text-secondary text-xl">Objectif : {project.objective}</span>
-                            <span className="text-secondary text-xl">Description : {project.description}</span>
-                            <a className="text-white flex items-center gap-3 bg-secondary md:w-[40vw] lg:w-[40%] w-[30%] p-3 rounded-full" href={project.github_link}>
-                                <svg className="w-[10%] fill-white" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
-                                <p> Lien vers le dépôt Github </p>
-                            </a>
-                            <div className="grid grid-cols-5 w-[60vw] lg:w-[27vw] md:w-[50vw] gap-6 rounded">
+                            {project.github_org_link !== "" && project.organisation_description !== "" && (
+                                <div className="my-3 text-secondary text-xl flex flex-col gap-4">
+                                    <span>{project.organisation_description}</span>
+                                </div>
+                            )}
+                            <span className="text-secondary text-xl">{project.description}</span>
+                            {project.github_org_link !== "" && project.organisation_description !== "" && (
+                                <a href={project.github_org_link} className="text-white text-2xl text-left">
+                                    <div className="flex gap-2 items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30">
+                                            <g fill="#36c">
+                                                <path d="M20.437 2.69c-3.37 0-5.778 3.05-8.186 5.297.322 0 .804-.16 1.285-.16.803 0 1.605.16 2.408.48 1.284-1.283 2.568-2.727 4.494-2.727.963 0 2.087.48 2.89 1.123 1.605 1.605 1.605 4.174 0 5.78l-4.174 4.172c-.642.642-1.926 1.124-2.89 1.124-2.246 0-3.37-1.446-4.172-3.212l-2.086 2.087c1.284 2.408 3.21 4.173 6.1 4.173 1.926 0 3.69-.802 4.815-2.086l4.172-4.174c1.445-1.444 2.408-3.21 2.408-5.297-.32-3.53-3.53-6.58-7.063-6.58z"/>
+                                                <path d="M13.535 22.113l-1.444 1.444c-.64.642-1.925 1.124-2.89 1.124-.962 0-2.085-.48-2.888-1.123-1.605-1.605-1.605-4.334 0-5.778l4.174-4.175c.642-.642 1.926-1.123 2.89-1.123 2.246 0 3.37 1.605 4.172 3.21l2.087-2.087c-1.284-2.407-3.21-4.173-6.1-4.173-1.926 0-3.692.803-4.815 2.087L4.547 15.69c-2.73 2.73-2.73 7.063 0 9.63 2.568 2.57 7.062 2.73 9.47 0l3.05-3.05c-.482.162-.963.162-1.445.162-.803 0-1.445 0-2.087-.32z"/>
+                                            </g>
+                                        </svg>
+                                        <p>Lien vers la page Github de l'organisation</p>
+                                    </div>
+                                </a>
+                            )}
+                            <p className="text-white text-2xl text-left">Technologies et outils utilisés</p>
+                            <div className="grid grid-cols-5 w-[80%] lg:w-[35%] big-desktop:w-[40%] mid-tablet:w-[50%] gap-6 rounded">
                                 {project.tools.map((tool, index) => (
-                                    <div className="max-w-full fill-white bg-secondary rounded-full p-2" key={index}>
+                                    <div className="max-w-full fill-white" key={index}>
                                         {React.createElement("div", {dangerouslySetInnerHTML: {__html: tool}})}
                                     </div>
                                 ))}
