f2fd8869703551cd09d5e488ad10fae8e44ad3ad
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-23 14:37:59 +0200

    :bug: Changement du lien de la photo de profil dans la Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import Information from "./Information.jsx";
 const Sidebar = () => {
     return (
         <div className="flex flex-col justify-items-center gap-7 max-h-screen m-auto">
-            <img className="rounded-full w-[50%] mx-auto" src="../../public/profile_pic.png" alt="Profile Picture"/>
+            <img className="rounded-full w-[50%] mx-auto" src="https://i.ibb.co/rQbsBQj/profile-pic.png" alt="Profile Picture"/>
             <Information />
         </div>
     );
