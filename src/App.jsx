03b4854d8aeb037741cfe3695f91258adc79faf7
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-25 22:51:19 +0200

    :lock: Suppression du console.log de la clé secrète

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,6 @@ const App = () => {
 
     const secretRoute = import.meta.env.VITE_REACT_SECRET_ROUTE;
 
-    console.log(secretRoute);
-
     return (
         <div className="bg-[#101214]">
             <BrowserRouter>
