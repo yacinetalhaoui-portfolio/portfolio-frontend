30585a9ec58c39e9e25ddb7180b6c40f9be90b19
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-25 22:54:50 +0200

    :bug: Correction des routes de la page Admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const Admin = () => {
+
+    const secretRoute = import.meta.env.VITE_REACT_SECRET_ROUTE;
+
     return (
         <div className="h-screen text-white text-2xl gap-8 flex flex-col items-center justify-center">
-            <Link to="/admin/add-project">Ajouter un projet</Link>
-            <Link to="/admin/add-experience">Ajouter une expérience professionnelle</Link>
-            <Link to="/admin/add-education">Ajouter une formation</Link>
+            <Link to={`/${secretRoute}/add-project`}>Ajouter un projet</Link>
+            <Link to={`/${secretRoute}/add-experience`}>Ajouter une expérience professionnelle</Link>
+            <Link to={`/${secretRoute}/add-education`}>Ajouter une formation</Link>
         </div>
     );
 };
