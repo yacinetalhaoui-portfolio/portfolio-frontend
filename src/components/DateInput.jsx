4ec317b7f25346b738b577a45fab96a3f3e822de
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-27 14:02:50 +0200

    :sparkles: Ajout d'un input permettant de créer d'ajouter des dates dans le formulaire

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -10,7 +10,7 @@ const DateInput = ({ value, onChange }) => {
         <div className="flex space-x-2">
             <input
                 type="month"
-                className="border border-gray-300 rounded-lg p-2"
+                className="border border-gray-300 rounded-lg p-2 w-full bg-gray-50 border text-black border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                 value={value.month}
                 onChange={handleMonthChange}
             />
