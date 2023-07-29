4793bdd4d28aa59eff2ee708d4aae6657b787e29
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-29 17:20:36 +0200

    :lipstick: Modification de la navbar pour l'afficher sous forme d'un menu burger sur mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,77 @@
-import {Link} from "react-router-dom";
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import useMobile from "../utils/useMobile.js";
 
 const Navbar = () => {
-    return (
-        <div className="text-[#7A7C80] text-xl flex justify-around items-center text-center h-32">
-            <Link className="hover:text-white transition ease-in" to="/about">À propos de moi</Link>
-            <Link className="hover:text-white transition ease-in" to="/experiences">Expériences</Link>
-            <Link className="hover:text-white transition ease-in" to="/portfolio">Portfolio</Link>
-        </div>
-    );
+    const isMobile = useMobile();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const renderMobile = () => {
+        return (
+            <div className="fixed top-0 left-0 w-full bg-white z-50 bg-[#1e2124] text-secondary">
+                <div className="text-[#7A7C80] text-xl flex gap-8 md:flex md:justify-between md:items-center md:h-16 p-4">
+                    <div className="md:hidden">
+                        <button onClick={handleMenuToggle} className="block text-xl">
+                            ☰
+                        </button>
+                    </div>
+
+                    <div className="md:text-center md:w-1/4 md:p-0">
+                        <h1>Yacine Talhaoui</h1>
+                    </div>
+                </div>
+
+                <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+                    <nav className={`p-4 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`}>
+                        <Link
+                            className="block py-2 text-center hover:text-white transition ease-in"
+                            to="/about"
+                            onClick={handleMenuToggle}
+                        >
+                            À propos de moi
+                        </Link>
+                        <Link
+                            className="block py-2 text-center hover:text-white transition ease-in"
+                            to="/experiences"
+                            onClick={handleMenuToggle}
+                        >
+                            Expériences
+                        </Link>
+                        <Link
+                            className="block py-2 text-center hover:text-white transition ease-in"
+                            to="/portfolio"
+                            onClick={handleMenuToggle}
+                        >
+                            Portfolio
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+        )
+    }
+
+    const renderDesktop = () => {
+        return (
+            <div className="text-[#7A7C80] text-xl md:flex md:justify-around md:items-center md:h-32">
+                <div className='md:w-3/4 md:text-right md:p-4'>
+                    <nav className="md:flex md:justify-around">
+                        <Link className="block md:inline-block md:px-4 py-2 hover:text-white transition ease-in"
+                              to="/about">À propos de moi</Link>
+                        <Link className="block md:inline-block md:px-4 py-2 hover:text-white transition ease-in"
+                              to="/experiences">Expériences</Link>
+                        <Link className="block md:inline-block md:px-4 py-2 hover:text-white transition ease-in"
+                              to="/portfolio">Portfolio</Link>
+                    </nav>
+                </div>
+            </div>
+        )
+    }
+
+    return isMobile ? renderMobile() : renderDesktop();
 };
 
 export default Navbar;
