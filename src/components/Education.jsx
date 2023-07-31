41c9a4f3a4f21f96fb87b8cd207fc9bc9ae9fb91
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-07-31 20:01:50 +0200

    :lipstick: Petit erreur dans le CSS (oublie des fonctions renderMobile et renderDesktop dans deux composants)

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import { experience_service } from "../services/experience.service.js";
+import {experience_service} from "../services/experience.service.js";
 import Loader from "./Loader.jsx";
-import { date_service } from "../services/date.service.js";
+import {date_service} from "../services/date.service.js";
 import apiService from "../services/api.service.js";
+import useMobile from "../utils/useMobile.js";
 
 const Education = () => {
+    const isMobile = useMobile();
     const [education, setEducation] = useState([]);
 
     useEffect(() => {
@@ -20,36 +22,75 @@ const Education = () => {
         }
     };
 
-    if (education.length === 0) {
+
+
+    const renderMobile = () => {
+        if (education.length === 0) {
+            return (
+                <div className="flex flex-col p-10 gap-16 max-w-screen-sm">
+                    <div>
+                        <h1 className="text-2xl font-bold text-white">Expériences universitaires</h1>
+                        <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
+                    </div>
+                    <Loader/>
+                </div>
+            )
+        }
+
         return (
             <div className="flex flex-col p-10 gap-16 max-w-screen-sm">
                 <div>
                     <h1 className="text-2xl font-bold text-white">Expériences universitaires</h1>
                     <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
                 </div>
-                <Loader/>
+                {education.map((item, index) => (
+                    <div key={index} className="flex flex-col gap-4">
+                        <div className="flex flex-col w-full">
+                            <img src={item.image_url} alt={item.school} className="w-36 mb-6 "/>
+                            <h1 className="text-lg text-white">{item.school}</h1>
+                            <h2 className="text-base text-secondary">{item.degree}</h2>
+                            <h3 className="text-base text-secondary">{date_service.createDateText(item.begin_date, item.end_date)}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const renderDesktop = () => {
+        if (education.length === 0) {
+            return (
+                <div className="flex flex-col p-10 gap-16 w-[90%]">
+                    <div>
+                        <h1 className="text-4xl font-bold text-white">Expériences universitaires</h1>
+                        <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
+                    </div>
+                    <Loader/>
+                </div>
+            )
+        }
+
+        return (
+            <div className="flex flex-col p-10 gap-16 w-[90%]">
+                <div>
+                    <h1 className="text-4xl font-bold text-white">Expériences universitaires</h1>
+                    <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
+                </div>
+                {education.map((item, index) => (
+                    <div key={index} className="flex flex-col gap-4">
+                        <div className="flex flex-col w-full">
+                            <img src={item.image_url} alt={item.school} className="w-48 mb-6 "/>
+                            <h1 className="text-xl text-white">{item.school}</h1>
+                            <h2 className="text-lg text-secondary">{item.degree}</h2>
+                            <h3 className="text-lg text-secondary">{date_service.createDateText(item.begin_date, item.end_date)}</h3>
+                        </div>
+                    </div>
+                ))}
             </div>
         )
     }
 
-    return (
-        <div className="flex flex-col p-10 gap-16 max-w-screen-sm">
-            <div>
-                <h1 className="text-2xl font-bold text-white">Expériences universitaires</h1>
-                <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
-            </div>
-            {education.map((item, index) => (
-                <div key={index} className="flex flex-col gap-4">
-                    <div className="flex flex-col w-full">
-                        <img src={item.image_url} alt={item.school} className="w-36 mb-6 "/>
-                        <h1 className="text-lg text-white">{item.school}</h1>
-                        <h2 className="text-base text-secondary">{item.degree}</h2>
-                        <h3 className="text-base text-secondary">{date_service.createDateText(item.begin_date, item.end_date)}</h3>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+    return isMobile ? renderMobile() : renderDesktop();
 };
 
 export default Education;
--- src/components/ProfessionalExperiences.jsx
+++ src/components/ProfessionalExperiences.jsx
@@ -2,8 +2,10 @@ import React, {useEffect, useState} from 'react';
 import {experience_service} from "../services/experience.service.js";
 import Loader from "./Loader.jsx";
 import {date_service} from "../services/date.service.js";
+import useMobile from "../utils/useMobile.js";
 
 const ProfessionalExperiences = () => {
+    const isMobile = useMobile();
     const [experience, setExperience] = useState([]);
 
     useEffect(() => {
@@ -19,44 +21,89 @@ const ProfessionalExperiences = () => {
         }
     };
 
-    if (experience.length === 0) {
+    const renderMobile = () => {
+        if (experience.length === 0) {
+            return (
+                <div className="flex flex-col p-10 gap-16 max-w-screen-sm">
+                    <div>
+                        <h1 className="text-2xl font-bold text-white">Expériences professionnelles</h1>
+                        <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
+                    </div>
+                    <Loader/>
+                </div>
+            )
+        }
+
         return (
             <div className="flex flex-col p-10 gap-16 max-w-screen-sm">
                 <div>
                     <h1 className="text-2xl font-bold text-white">Expériences professionnelles</h1>
                     <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
                 </div>
-                <Loader/>
+                {experience.map((item, index) => {
+
+                    return (
+                        <div key={index} className="flex flex-col gap-4">
+                            <div className="flex flex-col">
+                                <img src={item.image_url} alt={item.company}
+                                     className={item.company === "BECOMTECH" ? "w-36 h-auto mb-6 invert" : "w-36 h-auto mb-6  "}/>
+                                <h1 className="text-lg text-white">{item.company}, {item.title}</h1>
+                                <h3 className="text-base text-secondary">{date_service.createDateText(item.begin_date, item.end_date)} | {item.city}</h3>
+                                <ul className="list-disc ml-6 text-secondary mt-4">
+                                    {item.responsabilities.map((responsibility, index) => (
+                                        <li key={index} >{responsibility}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
 
-    return (
-        <div className="flex flex-col p-10 gap-16 max-w-screen-sm">
-            <div>
-                <h1 className="text-2xl font-bold text-white">Expériences professionnelles</h1>
-                <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
-            </div>
-            {experience.map((item, index) => {
-
-                return (
-                    <div key={index} className="flex flex-col gap-4">
-                        <div className="flex flex-col">
-                            <img src={item.image_url} alt={item.company}
-                                 className={item.company === "BECOMTECH" ? "w-36 h-auto mb-6 invert" : "w-36 h-auto mb-6  "}/>
-                            <h1 className="text-lg text-white">{item.company}, {item.title}</h1>
-                            <h3 className="text-base text-secondary">{date_service.createDateText(item.begin_date, item.end_date)} | {item.city}</h3>
-                            <ul className="list-disc ml-6 text-secondary mt-4">
-                                {item.responsabilities.map((responsibility, index) => (
-                                    <li key={index} >{responsibility}</li>
-                                ))}
-                            </ul>
-                        </div>
+    const renderDesktop = () => {
+        if (experience.length === 0) {
+            return (
+                <div className="flex flex-col p-10 gap-16 w-[90%]">
+                    <div>
+                        <h1 className="text-4xl font-bold text-white">Expériences professionnelles</h1>
+                        <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
                     </div>
-                )
-            })}
-        </div>
-    );
+                    <Loader/>
+                </div>
+            )
+        }
+
+        return (
+            <div className="flex flex-col p-10 gap-16 w-[90%]">
+                <div>
+                    <h1 className="text-4xl font-bold text-white">Expériences professionnelles</h1>
+                    <hr className="mt-4 border-t-2 border-gray-300 w-[80%]"/>
+                </div>
+                {experience.map((item, index) => {
+
+                    return (
+                        <div key={index} className="flex flex-col gap-4">
+                            <div className="flex flex-col">
+                                <img src={item.image_url} alt={item.company}
+                                     className={item.company === "BECOMTECH" ? "w-52 h-auto mb-6 invert" : "w-52 h-auto mb-6  "}/>
+                                <h1 className="text-xl text-white">{item.company}, {item.title}</h1>
+                                <h3 className="text-lg text-secondary">{date_service.createDateText(item.begin_date, item.end_date)} | {item.city}</h3>
+                                <ul className="list-disc ml-6 text-secondary mt-4">
+                                    {item.responsabilities.map((responsibility, index) => (
+                                        <li key={index} >{responsibility}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    return isMobile ? renderMobile() : renderDesktop();
 };
 
 export default ProfessionalExperiences;
